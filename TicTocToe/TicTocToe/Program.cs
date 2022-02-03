be3005d18d75910cd82c6916c8a1223b6416efc3
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-02-03 10:22:10 +0530

    [Vinayak] creating board

---
+++ TicTocToe/TicTocToe/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTocToe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Tic Tok Toe Game");
+            TicTokToeGame.Game();
+            Console.ReadLine();
+        }
+    }
+}
